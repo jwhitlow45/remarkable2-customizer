7593f7c109da783a291012a2afb600581283fe9a
jwhitlow45 <jwhitlow45@gmail.com>
2023-12-10 13:52:07 -0800

    Pass through FileNotFoundException and add logs for operations on splash screens

--- splashes/file_transfer.py
+++ splashes/file_transfer.py
@@ -24,6 +24,8 @@ class FileTransfer:
         try:
             self.sftp.put(localpath=local_path, remotepath=remote_path)
             return True
+        except FileNotFoundError as e:
+            raise e
         except Exception as e:
             logging.error("Error when trying to put file on remote: %s", e)
             return False
@@ -32,37 +34,54 @@ class FileTransfer:
         try:
             self.sftp.get(remotepath=remote_path, localpath=local_path)
             return True
+        except FileNotFoundError as e:
+            raise e
         except Exception as e:
             logging.error("Error when trying to get file from remote: %s", e)
             return False
 
-    def backup_splash_screen(self, splash_screen: SplashScreen) -> None:
-        return self._get_file(
+    @staticmethod
+    def _log(success: bool, splash_screen: SplashScreen, operation: str) -> None:
+        if success:
+            logging.info("%s %s success.", splash_screen.value, operation)
+        else:
+            logging.error("%s %s failed.", splash_screen.value, operation)
+
+    def backup_splash_screen(self, splash_screen: SplashScreen) -> bool:
+        success = self._get_file(
             SplashScreenPath[splash_screen.value][REMOTE],
             SplashScreenPath[splash_screen.value][BACKUP],
         )
+        FileTransfer._log(success, splash_screen, "back up")
+        return success
 
-    def restore_splash_screen_from_backup(self, splash_screen: SplashScreen) -> None:
-        return self._put_file(
+    def restore_splash_screen_from_backup(self, splash_screen: SplashScreen) -> bool:
+        success = self._put_file(
             SplashScreenPath[splash_screen.value][BACKUP],
             SplashScreenPath[splash_screen.value][REMOTE],
         )
+        FileTransfer._log(success, splash_screen, "restore from backup")
+        return success
 
-    def restore_splash_screen_from_default(self, splash_screen: SplashScreen) -> None:
-        return self._put_file(
+    def restore_splash_screen_from_default(self, splash_screen: SplashScreen) -> bool:
+        success = self._put_file(
             SplashScreenPath[splash_screen.value][DEFAULT],
             SplashScreenPath[splash_screen.value][REMOTE],
         )
+        FileTransfer._log(success, splash_screen, "restore from default")
+        return success
 
     def set_custom_splash_screen(
         self, splash_screen: SplashScreen, path_to_splash: str
-    ) -> None:
+    ) -> bool:
         if not path_to_splash:
             raise ValueError(
                 "Local file path to custom splash screen must be provided."
             )
         if path_to_splash.split(".")[-1] != PNG_EXT[1:]:
             raise ValueError("Splash screen must be a .png file.")
-        return self._put_file(
+        success = self._put_file(
             path_to_splash, SplashScreenPath[splash_screen.value][REMOTE]
         )
+        FileTransfer._log(success, splash_screen, "set custom splash screen")
+        return success
